b3ce46b493888512fbe4a9bf0da67cd4702d5f15
Danylo Zherdiev <zerdevdanil430@gmail.com>
2023-11-23 13:21:06 +0200

    initial commit

---
+++ src/main/java/com/example/CostControl/CostControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.CostControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostControlApplication.class, args);
+	}
+
+}
