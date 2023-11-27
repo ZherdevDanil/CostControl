c66206090c7d10fbf7c1cf1f0c116dc0c1e4e934
Danylo Zherdiev <zerdevdanil430@gmail.com>
2023-11-27 11:52:59 +0200

    create class GenerateRandomValue

---
+++ src/main/java/com/example/CostControl/Util/GenerateRandomValue.java
@@ -0,0 +1,23 @@
+package com.example.CostControl.Util;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+
+@Component
+public class GenerateRandomValue {
+    public long generateRandomUniqueNumber(List<Long> listOfId){
+        long newUniqueUserId = new Random().nextLong() % 1001;
+        if (newUniqueUserId < 0) {
+            newUniqueUserId = -newUniqueUserId;
+        }
+        if (listOfId.contains(newUniqueUserId)){
+            generateRandomUniqueNumber(listOfId);
+        }
+        return newUniqueUserId;
+    }
+}
