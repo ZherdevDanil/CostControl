3f2d9b43cefaf1bdd6fa55cb1a02c75c5847e9a2
Danylo Zherdiev <zerdevdanil430@gmail.com>
2023-11-27 11:55:44 +0200

    add some data in List<User>, List<Category>, List<Record>

--- src/main/java/com/example/CostControl/Util/FakeDataBase.java
+++ src/main/java/com/example/CostControl/Util/FakeDataBase.java
@@ -5,6 +5,8 @@ import com.example.CostControl.Entity.Record;
 import com.example.CostControl.Entity.User;
 import org.springframework.stereotype.Component;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +17,18 @@ public class FakeDataBase {
     private List<Record> records = new ArrayList<>();
 
     private List<Category> categories = new ArrayList<>();
+
+    public FakeDataBase() throws ParseException {
+        users.add(new User(1L,"Jack"));
+        users.add(new User(5L,"Billy"));
+
+        categories.add(new Category(2L,"Utilities"));
+        categories.add(new Category(4L,"Groceries"));
+
+
+        records.add(new Record(1L,users.get(0).getId(),categories.get(0).getId(),new SimpleDateFormat("dd.MM.yyyy HH:mm").parse("24.11.2023 12:35"),374.34));
+
+    }
 
 
     public List<User> getUsers() {
