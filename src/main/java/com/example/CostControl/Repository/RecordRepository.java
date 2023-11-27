f0fabb0745ee094d9f2e5d1d038ae0c74cf861a4
Danylo Zherdiev <zerdevdanil430@gmail.com>
2023-11-27 17:12:40 +0200

    getListOfRecordId(),findRecordsByUserIdAndCategoryId()

--- src/main/java/com/example/CostControl/Repository/RecordRepository.java
+++ src/main/java/com/example/CostControl/Repository/RecordRepository.java
@@ -1,8 +1,15 @@
 package com.example.CostControl.Repository;
 
+import com.example.CostControl.Entity.Category;
 import com.example.CostControl.Entity.Record;
+import com.example.CostControl.Entity.User;
+import com.example.CostControl.Exception.NotFoundRecordsException;
 import com.example.CostControl.Util.FakeDataBase;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class RecordRepository {
@@ -12,23 +19,78 @@ public class RecordRepository {
         this.fakeDataBase = fakeDataBase;
     }
 
-    public Record findById(long id){
+    public Optional<Record> findById(long id){
+        return fakeDataBase.getRecords().stream().filter(record -> record.getId()==id).findFirst();
+        /*
         Record record = new Record();
         for (Record record1: fakeDataBase.getRecords()) {
             if (record1.getId()==id){
                 record=record1;
             }
         }
-        return record;
+        return record;*/
     }
 
 
     public void deleteById(long id){
-        fakeDataBase.getRecords().remove(findById(id));
+        findById(id).ifPresent(fakeDataBase.getRecords()::remove);
+        /*fakeDataBase.getRecords().remove(findById(id));*/
     }
 
 
-    public void save(Record record){
+    public Record save(Record record){
         fakeDataBase.getRecords().add(record);
+        return record;
+
+    }
+
+    public List<Record> findRecordsByUserId(long userId) {
+        List<Record> records = new ArrayList<>();
+        for (Record record : fakeDataBase.getRecords()) {
+            if (record.getUserId() == userId) {
+                records.add(record);
+            }
+        }
+        if (records.isEmpty()){
+            throw new NotFoundRecordsException("userId",userId);
+        }
+        return records;
+    }
+
+    public List<Record> findRecordsByCategoryId(long categoryId){
+        List<Record> records = new ArrayList<>();
+        for (Record record : fakeDataBase.getRecords()) {
+            if (record.getCategoryId() == categoryId) {
+                records.add(record);
+            }
+        }
+        if (records.isEmpty()){
+            throw new NotFoundRecordsException("categoryId",categoryId);
+        }
+        return records;
+    }
+
+    public List<Record> findRecordsByUserIdAndCategoryId(long userId,long categoryId){
+        List<Record> records = new ArrayList<>();
+        for (Record record:fakeDataBase.getRecords()) {
+            if (record.getUserId() == userId){
+                if (record.getCategoryId() == categoryId){
+                    records.add(record);
+                }
+            }
+        }
+        if (records.isEmpty()){
+           throw new NotFoundRecordsException(userId,categoryId);
+        }
+        return records;
+    }
+
+
+    public List<Long> getListOfRecordId(){
+        List<Long> list=new ArrayList<>();
+        for (Record record:fakeDataBase.getRecords()) {
+            list.add(record.getId());
+        }
+        return list;
     }
 }
--- src/main/java/com/example/CostControl/Service/RecordService.java
+++ src/main/java/com/example/CostControl/Service/RecordService.java
@@ -1,29 +1,51 @@
 package com.example.CostControl.Service;
 
 import com.example.CostControl.Entity.Record;
+import com.example.CostControl.Exception.RecordNotFoundException;
 import com.example.CostControl.Repository.RecordRepository;
+import com.example.CostControl.Util.GenerateRandomValue;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RecordService {
     private final RecordRepository recordRepository;
 
-    public RecordService(RecordRepository recordRepository) {
+    private final GenerateRandomValue generateRandomValue;
+
+    public RecordService(RecordRepository recordRepository,GenerateRandomValue generateRandomValue) {
         this.recordRepository = recordRepository;
+        this.generateRandomValue=generateRandomValue;
     }
 
     public Record getRecordById(long id){
-        return recordRepository.findById(id);
+        return recordRepository.findById(id).orElseThrow(()->new RecordNotFoundException(id));
     }
 
     public void deleteRecordById(long id){
         recordRepository.deleteById(id);
     }
 
-    public void addNewRecord(Record record){
+    public Record addNewRecord(Record record){
         recordRepository.save(record);
+        return record;
     }
 
+    public List<Record> getRecordsByCategoryId(long categoryId){
+        return recordRepository.findRecordsByCategoryId(categoryId);
+    }
+
+    public List<Record> getRecordsByUserId(long userId){
+        return recordRepository.findRecordsByUserId(userId);
+    }
+
+    public List<Record> getRecordsByUserIdAndCategoryId(long userId , long categoryId){
+        return recordRepository.findRecordsByUserIdAndCategoryId(userId, categoryId);
+    }
+    public long generateNewUniqueRecordId(){
+        return generateRandomValue.generateRandomUniqueNumber(recordRepository.getListOfRecordId());
+    }
 
 
 }
